6a819d5921936b90fed35634a0dcb5c7c1b35e79
Tal Glanzman <talglanzman@gmail.com>
2023-02-15 21:11:58 +0200

    remove homePath

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,8 +1,6 @@
-import config from '../config.json';
-
 const url = (...values: string[]) => values.join("/");
 
-export const home = () => config.site.homePath;
+export const home = () => "";
 export const taxonomyArticles = (taxonomy: string, value: string) => url(home(), `_${taxonomy.toLowerCase()}`, value);
 export const tagArticles = (tag: string) => taxonomyArticles('tags', tag);
 export const categroyArticles = (category: string) => taxonomyArticles('categories', category);
