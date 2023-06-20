53b74488189a78b390a236dbd1a9a8b759e759ce
Tal Glanzman <talglanzman@gmail.com>
2023-06-20 12:19:23 +0300

    move out the content

--- next-legacy/lib/content-cache.ts
+++ next-legacy/lib/content-cache.ts
@@ -9,7 +9,7 @@ import { readContent, readContentInfo } from './content';
 
 import config from '../config.json';
 
-const CACHE_ENABLED = false;
+const CACHE_ENABLED = true;
 
 const ContentDirectory = path.join(process.cwd(), config.content.path);
 
