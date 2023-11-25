4568ae10afa20eda8c688d9b6b7d95d73657a86f
Tal Glanzman <talglanzman@gmail.com>
2023-11-25 15:57:34 +0200

    try to fix static routing

--- personal-site/src/lib/urls.ts
+++ personal-site/src/lib/urls.ts
@@ -1,9 +1,9 @@
 const url = (...values: string[]) => values.join("/");
 
 export const home = () => "/";
-export const tag = (tag: string) => url('/tags', tag);
+export const tag = (tag: string) => url('/tags', encodeURIComponent(tag));
 export const categories = () => url('/categories');
-export const category = (category: string) => url('/categories', category);
+export const category = (category: string) => url('/categories', encodeURIComponent(category));
 export const articles = () => url('/articles');
-export const article = (articleId: string) => url('/articles', articleId);
+export const article = (articleId: string) => url('/articles', encodeURI(articleId));
 export const search = (searchQuery: string) => url(home(), `_search?query=${searchQuery}`);
