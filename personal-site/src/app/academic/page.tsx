4833965b13b48f763acd6fa12020209c643dbea2
Tal Glanzman <talglanzman@gmail.com>
2025-08-17 11:43:59 +0300

    update

--- personal-site/src/app/academic/page.tsx
+++ personal-site/src/app/academic/page.tsx
@@ -12,6 +12,23 @@ const items = [
       url: "https://drive.google.com/file/d/1IxUX7kaJxeI-nedcWuEV97yMABppiiHL/view?usp=drive_link",
     }]
   }, {
+    title: "Classical vs. Quantum Algorithms for Solving the Heat Equation",
+    description: "A comparison of algorithms for simulating the d-dimensional heat equation",
+    links: [{
+      name: "Paper",
+      url: "https://drive.google.com/file/d/1UxQDKG_qQAwLHhB0qjfFWiqHxohIoSWv/view?usp=sharing",
+    }, {
+      name: "Slides",
+      url: "https://drive.google.com/file/d/18Zg4770RkZaYNruh108LONxhLbtKmOrQ/view?usp=sharing",
+    }]
+  }, {
+    title: "Locally-iterative coloring in CONGEST networks",
+    description: "Distributed algorithm for coloring network under the CONGEST model",
+    links: [{
+      name: "Colloquium",
+      url: "https://drive.google.com/file/d/1dW0HBdVFPLuo6E18CphcmU7EZ6o_CJyD/view?usp=sharing",
+    }]
+  }, {
     title: "Tarski's Planks Problem (Hebrew)",
     description: "An abstract result in convex functional analysis",
     links: [{
@@ -21,8 +38,7 @@ const items = [
       name: "Slides",
       url: "https://drive.google.com/file/d/1aNiphLlYF1MR9RSB7unQmLu9vJSvh8uQ/view?usp=drive_link",
     }]
-  },
-]
+  }]
 
 type ItemProps = {
   item: {
@@ -55,6 +71,14 @@ export default async function Academic() {
   return (
     <div className={styles.container}>
       <h2>Academic Work</h2>
+
+      <p>Interests:</p>
+      <ul>
+        <li>Parallel, Graph and Optimizaton Algorithms</li>
+        <li>AI/ML, Scientific and Quantum Computation</li>
+        <li>Mathematics</li>
+        <li>Accelerated Computing</li>
+      </ul>
 
       <div className={styles.items}>
         {items.map((item, idx) => (
